3576df101c58136269eb24605f9c7572a42a31e2
shayro9 <shayro13@gmail.com>
2024-08-03 01:48:09 +0300

    Update server.c

--- server.c
+++ server.c
@@ -15,15 +15,15 @@
 struct Queues_container{
     struct Queue * m_waiting_ptr;
     struct Queue * m_running_ptr;
-    threads_stats m_stats;
+    int index;
 };
 // HW3: Parse the new arguments too
 void getargs(int *port, int *pool_size, int *queue_size, char **schedalg, int argc, char *argv[])
 {
     //TODO: ADD VALIDATION
     if (argc < 2) {
-	    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-	    exit(1);
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(1);
     }
     if(argc < 5) exit(1);
 
@@ -35,7 +35,7 @@ void getargs(int *port, int *pool_size, int *queue_size, char **schedalg, int ar
 
 void *thread_function(void* Container);
 
-pthread_cond_t cnd, master_cnd, empty_cnd; 
+pthread_cond_t cnd, master_cnd, empty_cnd;
 pthread_mutex_t mtx;
 
 int main(int argc, char *argv[])
@@ -46,11 +46,7 @@ int main(int argc, char *argv[])
 
     struct Queue * waiting_ptr = createQueue();
     struct Queue * running_ptr = createQueue();
-    struct Queues_container container = {
-            .m_waiting_ptr = waiting_ptr,
-            .m_running_ptr = running_ptr,
-            .m_stats = (threads_stats)malloc(sizeof(*container.m_stats)),
-    };
+
     getargs(&port, &pool_size, &queue_size, &schedalg, argc, argv);
 
     pthread_mutex_init(&mtx, NULL);
@@ -59,14 +55,18 @@ int main(int argc, char *argv[])
     pthread_cond_init(&empty_cnd, NULL);
 
     pthread_t *thread_pool = (pthread_t *)malloc(pool_size * sizeof(pthread_t));
+    struct Queues_container *containers = (struct Queues_container *)malloc(pool_size * sizeof(struct Queues_container));
     for (int i = 0; i < pool_size; i++) {
-        container.m_stats->id = i;
-        if (pthread_create(&thread_pool[i], NULL, thread_function, &container) != 0) {
+        containers[i].m_waiting_ptr = waiting_ptr;
+        containers[i].m_running_ptr = running_ptr;
+        containers[i].index = i;
+
+        if (pthread_create(&thread_pool[i], NULL, thread_function, &containers[i]) != 0) {
             perror("pthread_create failed");
             exit(EXIT_FAILURE);
         }
     }
-    
+
     listenfd = Open_listenfd(port);
     while (1) {
         clientlen = sizeof(clientaddr);
@@ -89,10 +89,10 @@ int main(int argc, char *argv[])
                 gettimeofday(&arrival_time, NULL);
                 pthread_mutex_lock(&mtx);
             }
-            
+
             else if(strcmp(schedalg, "dh") == 0){
                 Request_info oldest_request = popQueue(waiting_ptr);
-                if(oldest_request.fd == 0){ 
+                if(oldest_request.fd == 0){
                     continue;
                 }
                 Request_info val = {connfd, arrival_time};
@@ -103,13 +103,13 @@ int main(int argc, char *argv[])
                 gettimeofday(&arrival_time, NULL);
                 pthread_mutex_lock(&mtx);
             }
-            
+
             else if(strcmp(schedalg, "bf") == 0){
                 while(isEmpty(waiting_ptr) == 0 && isEmpty(running_ptr) == 0){
                     pthread_cond_wait(&empty_cnd, &mtx);
                 }
             }
-            
+
             else if(strcmp(schedalg, "random") == 0){
                 int size = 0;
                 int * removed = delete_random(waiting_ptr, &size);
@@ -128,13 +128,13 @@ int main(int argc, char *argv[])
         enQueue(waiting_ptr, request);
         pthread_cond_signal(&cnd);
         pthread_mutex_unlock(&mtx);
-	    //Close(connfd);
-	// 
-	// HW3: In general, don't handle the request in the main thread.
-	// Save the relevant info in a buffer and have one of the worker threads 
-	// do the work. 
-	// 
-	//requestHandle(connfd);
+        //Close(connfd);
+        //
+        // HW3: In general, don't handle the request in the main thread.
+        // Save the relevant info in a buffer and have one of the worker threads
+        // do the work.
+        //
+        //requestHandle(connfd);
     }
 
 }
@@ -143,12 +143,15 @@ void *thread_function(void* Container){
     struct Queues_container* container = (struct Queues_container*)Container;
     struct Queue * waiting_ptr = container->m_waiting_ptr;
     struct Queue * running_ptr = container->m_running_ptr;
-    threads_stats t_stats = container->m_stats;
+    int id = container->index;
+
+    struct Threads_stats temp = {id,0,0,0};
+    threads_stats t_stats = &temp;
     struct timeval working_time; // the time the thread started to work on the request
     while(1){
         pthread_mutex_lock(&mtx);
         while(isEmpty(waiting_ptr) == 1){
-           /// printf("%ld Waiting\n", pthread_self());
+            //printf("%ld Waiting\n", pthread_self());
             pthread_cond_wait(&cnd, &mtx);
         }
         Request_info top_request_info = popQueue(waiting_ptr);
@@ -158,7 +161,7 @@ void *thread_function(void* Container){
         int top_request = top_request_info.fd;
         enQueue(running_ptr, top_request_info);
         pthread_mutex_unlock(&mtx);
-        
+
         //sleep(10);
         requestHandle(top_request_info, waiting_ptr, running_ptr, t_stats, working_time);
         pthread_mutex_lock(&mtx);
@@ -172,8 +175,3 @@ void *thread_function(void* Container){
         Close(top_request);
     }
 }
-
-    
-
-
- 
